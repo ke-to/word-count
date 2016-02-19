656079d626bec2b6fc9d55df582771d65968a664
ke-to <kuronokei0606@gmail.com>
2016-02-20 01:23:11 +0900

    output TXT file

--- word-count.py
+++ word-count.py
@@ -1,31 +1,35 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import os
-import requests
 from collections import Counter
 import sys, codecs
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+import csv
 
-fileName = raw_input("Input CSV file name write now! > ")
+fileName = input("Output CSV file name write now! > ")
 
-result = open(fileName, 'w')
-result.write('')
+result = open(fileName, 'w', newline='')
+csvWriter = csv.writer(result)
+csvWriter.writerow(["count","word"]) 
+
+fileName2 = input("Input TXT file name write now! > ")
 
 container=u""
-f = open('/Users/kojimakeito/Desktop/result2.txt','r')
+f = open(fileName2,'r')
 for line in f:
-	container += line.decode('utf-8')
-	print line
+	container += line
+	#print line
 	
-print "container in contents"
+print("container in contents.")
+Clist = container.split(",")
+print("container split is finished.")
 
-counter = Counter(container)
+counter = Counter(Clist)
 for word, cnt in counter.most_common():
-	#print type(word)
-	text = word
-	result.writelines(text)
-	print text
+	print(type(cnt))
+	text = [cnt,word]
+	csvWriter.writerow(text) 
+	print(text)
 	
 result.close
 
-print "COMPLATE"
+print ("COMPLATE")
